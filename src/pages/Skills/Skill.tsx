49942d885fd092c06de702c219084e0c603ab93e
Abdul Hakam <zehas.11@gmail.com>
2022-12-08 13:12:04 +0500

    added logos to skills i guess

--- src/pages/Skills/Skill.tsx
+++
@@ -1,8 +0,0 @@
-import { Col, Typography } from "antd";
-
-export default function Skill(props : any) {
-  return (
-  <Col span={4}>
-    <Typography.Title level={3}>{props.children}</Typography.Title>
-  </Col>
-)}
--- src/pages/Skills/index.tsx
+++ src/pages/Skills/index.tsx
@@ -1,28 +1,34 @@
 import { Col, Row, Typography } from "antd";
-import React from "react"
-import Skill from "./Skill";
+import React from "react";
+import { ReactComponent as HTMLlogo } from '../../assets/images/html.svg'
+import { ReactComponent as CSSlogo } from '../../assets/images/css-3.svg'
+import { ReactComponent as JSlogo } from '../../assets/images/logo-js.svg'
+import { ReactComponent as TSlogo } from '../../assets/images/typescript.svg'
+import { ReactComponent as Reactlogo } from '../../assets/images/react.svg';
+import { ReactComponent as Muilogo } from '../../assets/images/mui.svg'
+import { ReactComponent as BS5logo} from '../../assets/images/bootstrap5.svg'
 
 const Skills: React.FC = () => (
   <Row justify='space-around' align='middle' style={{height: '100vh',flexFlow: 'column'}}>
     <Col style={{width:'100%',}}><Typography.Title style={{display:'flex',justifyContent:'center'}} level={1}> My Skills </Typography.Title></Col>
     <Row justify='space-around'>
       <Typography.Title style={{display:'block',width:'100%'}} level={2}>Languages</Typography.Title>
-      <Skill >
-        <img width={128} src={require("../../assets/images/html.png")} alt=""/>HTML</Skill>
-      <Skill >
-        <img width={128} src={require("../../assets/images/css3.png")} alt=""/>CSS</Skill>
-      <Skill >
-        <img width={128} src={require("../../assets/images/js.png")} alt=""/> Javascript</Skill>
-      <Skill >
-        <img width={128} src={require("../../assets/images/ts.png")} alt=""/>Typescript</Skill>
+      <Col>
+        <HTMLlogo width={100} height={100}/><Typography.Title level={3} style={{display:'flex',justifyContent:'center'}}>HTML</Typography.Title></Col>
+      <Col >
+        <CSSlogo width={100} height={100}/><Typography.Title level={3} style={{display:'flex',justifyContent:'center'}}>CSS</Typography.Title></Col>
+      <Col >
+         <JSlogo width={100} height={100}/><Typography.Title level={3} style={{display:'flex',justifyContent:'center'}}>Javascript</Typography.Title></Col>
+      <Col >
+        <TSlogo width={100} height={100}/><Typography.Title level={3} style={{display:'flex',justifyContent:'center'}}>Typescript</Typography.Title></Col>
         
       <Typography.Title style={{display:'block',width:'100%'}} level={2}>Frameworks/ Libraries</Typography.Title>
-      <Skill >
-        <img width={128} src="../../assets/images/react.png" alt=""/>ReactJS</Skill>
-      <Skill >
-        {/*<img src="../../assets/images/" alt=""/>*/}Material UI</Skill>
-      <Skill >
-        <img width={128} src={require("../../assets/images/bootstrap.png")} alt=""/>Bootstrap</Skill>
+      <Col >
+        <Reactlogo width={128} height={128}/><Typography.Title level={3} style={{display:'flex',justifyContent:'center'}}>ReactJS</Typography.Title></Col>
+      <Col >
+        <Muilogo width={128} height={128}/> <Typography.Title level={3} style={{display:'flex',justifyContent:'center'}}>Material UI</Typography.Title></Col>
+      <Col >
+        <BS5logo width={100} height={100}/><Typography.Title level={3} style={{display:'flex',justifyContent:'center'}}>Bootstrap</Typography.Title></Col>
     </Row>
   </Row>
 )
